988dc17935afc12901a2a3a7e3c446029de1e2f0
cupid-qx <17865197338@163.com>
2017-02-08 16:28:17 +0800

    verson 1

--- Demo/app/src/main/java/com/cupido/demo/MainActivity.java
+++ Demo/app/src/main/java/com/cupido/demo/MainActivity.java
@@ -2,6 +2,7 @@ package com.cupido.demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +10,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("Demo","MainActivity onCreate...");
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Demo","MainActivity onStart...");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Demo","MainActivity onStop...");
     }
 }
--- VideoPlayer/app/src/main/java/com/cupido/videoplayer/MainActivity.java
+++ VideoPlayer/app/src/main/java/com/cupido/videoplayer/MainActivity.java
@@ -1,14 +1,42 @@
 package com.cupido.videoplayer;
 
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private VideoView videoView ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        videoView = (VideoView) findViewById(R.id.vv_main) ;
+
+        /**
+         * 本地视频播放
+         */
+
+//        String path = Environment.getExternalStorageDirectory().getAbsolutePath()+"/hello.mp4";
+//        videoView.setVideoPath(path);
+
+        /**
+         * 网络视频播放
+         */
+        videoView.setVideoURI(Uri.parse("http://192.168.1.106:8080/Video/hello.mp4"));
+
+        /**
+         * 视频的控制 MediaController
+         */
+
+        MediaController controller = new MediaController(this) ;
+        // 设置VideoView与MediaCOntroller 建立关联
+        videoView.setMediaController(controller);
+        // 设置MediaController 与 VideoView 建立关联
+        controller.setMediaPlayer(videoView);
 
     }
 }
