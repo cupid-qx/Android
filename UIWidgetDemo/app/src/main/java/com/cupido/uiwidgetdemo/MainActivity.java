4bae03e37d6e97deae1eac5e64db5853c2241278
cupid-qx <17865197338@163.com>
2017-02-26 14:20:00 +0800

    RecyclerView

--- UIWidgetDemo/app/src/main/java/com/cupido/uiwidgetdemo/MainActivity.java
+++ UIWidgetDemo/app/src/main/java/com/cupido/uiwidgetdemo/MainActivity.java
@@ -1,13 +1,47 @@
 package com.cupido.uiwidgetdemo;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.btn_click);;
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.btn_click:
+                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                dialog.setTitle("Warning") ;
+                dialog.setCancelable(false);
+                dialog.setMessage("Are you sure to delete") ;
+                dialog.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                dialog.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                dialog.show() ;
+                break;
+            default:
+                break;
+        }
     }
 }
